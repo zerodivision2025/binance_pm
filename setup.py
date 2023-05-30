8e08a48a87cda8853328b5d1d8468a9431cb339c
marz <wudanhit@163.com>
2023-05-30 13:21:08 +0800

    + add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+from setuptools import setup
+
+setup(
+    name='binance_pm_1.5',
+    version='0.0.1',
+    packages=['binance_pm_1.5'],
+    install_requires=['requests', 'pycryptodome'],
+    author='zerodivision',
+    author_email='zerodivision@zerodivision.tech',
+    url='https://www.zerodivision.tech/'
+)
