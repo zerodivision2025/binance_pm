95084b14269f5e9841064e478b6768f53f794361
marz <wudanhit@163.com>
2023-05-30 11:56:31 +0800

    + test

--- test/example.py
+++ test/example.py
@@ -2,4 +2,11 @@ from binance_pm.binance_pm import BinancePm
 
 if __name__ == '__main__':
     client = BinancePm('', '')
-    print(client.request_balance())
+    # print(client.request_balance())
+    # print(client.request_account())
+    # print(client.request_max_borrowable('BTC'))
+    # print(client.request_max_withdraw('BTC'))
+    # print(client.request_um_positions())
+    # print(client.request_cm_positions())
+
+    print(client.request_um_open_orders())
