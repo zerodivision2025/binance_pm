0ec98fe4141ba4256410842b65d1e191e6e4a27c
marz <wudanhit@163.com>
2023-06-19 14:24:24 +0800

    + um/cm income

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='binance_pm',
-    version='0.0.1',
+    version='0.0.2',
     packages=['binance_pm'],
     install_requires=['requests', 'pycryptodome'],
     author='zerodivision',
