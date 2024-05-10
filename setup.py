d5a04f1bec39d60da611ad308278b466d02a8488
marzwu <marzwu@gmail.com>
2024-05-10 12:49:08 +0800

    ! update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='binance_pm',
-    version='v0.0.3',
+    version='v0.0.7',
     packages=['binance_pm'],
     install_requires=['requests', 'pycryptodome'],
     author='zerodivision',
